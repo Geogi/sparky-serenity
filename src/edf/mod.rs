7a2253381efe620838b2ddd05c5de90c46a755f9
Paul Blouët <paul@paulblouet.fr>
2020-05-05 04:35:03 +0200

    edf!
    no "smart verse support", just a round robin with a timeout (it didn't seem right during testing).

--- src/edf/mod.rs
+++ src/edf/mod.rs
@@ -6,6 +6,9 @@ use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::CommandResult;
 use serenity::model::channel::Message;
 use typemap::Key;
+use chrono::{Utc, DateTime, Duration};
+use crate::edf::songs::SONGS;
+use anyhow::Context as _;
 
 #[group]
 #[prefix = "edf"]
@@ -13,9 +16,11 @@ use typemap::Key;
 pub struct Edf;
 
 struct SongFollowUp {
-    last_timestamp: u64,
-    song_id: u8,
+    last_time: DateTime<Utc>,
+    song_id: usize,
 }
+
+const FOLLOWUP_INTERVAL: i64 = 30;
 
 struct SongFollowUpKey;
 impl Key for SongFollowUpKey {
@@ -25,6 +30,24 @@ impl Key for SongFollowUpKey {
 #[command]
 fn sing(ctx: &mut Context, msg: &Message) -> CommandResult {
     wrap_cmd_err(|| {
+        let ctx = &*ctx;
+        let followup = {
+            let data = ctx.data.read();
+            data.get::<SongFollowUpKey>()
+                .filter(|sf| Utc::now() - sf.last_time < Duration::seconds(FOLLOWUP_INTERVAL))
+                .map(|sf| sf.song_id)
+        };
+        let next = followup.map(|id| (id + 1) % SONGS.len()).unwrap_or(0);
+        {
+            let mut data = ctx.data.write();
+            data.insert::<SongFollowUpKey>(SongFollowUp {
+                last_time: Utc::now(),
+                song_id: next,
+            });
+        }
+            let song = SONGS.get(next).context("wrong song id")?;
+            msg.channel_id.send_message(ctx, |m|
+                m.content(song))?;
         Ok(())
     })
 }
--- src/edf/songs.rs
+++ src/edf/songs.rs
@@ -1,18 +1,10 @@
-pub static SONGS: &[(&str, &str)] = &[
-    (
+pub static SONGS: [&str; 8] = [
         "Oh~ We are the valiant infantry. We are the alpha team with passion and camaraderie.",
         "Hear us as we shout at the top of our lungs. Be calm and raise your guns.",
-    ),
-    (
         "It's only with our sacrifice, that mankind can still exist down here in paradise.",
         "To defend our dearest Mother Earth, we're ready to give up our lives!",
-    ),
-    (
         "High up in the air our comrades fight, dashing through the sky now like a million bolt of light.",
-        "We shall spread our wings wide and fly high. Soaring, gliding through the endless sky!"
-    ),
-    (
+        "We shall spread our wings wide and fly high. Soaring, gliding through the endless sky!",
         "Now pick up our weapons, off we go. Firing at the flying saucers, shooting down our foe.",
-        "We shall not allow these aliens, to rule homo sapiens."
-    )
+        "We shall not allow these aliens, to rule homo sapiens.",
 ];
