9d0ae4265927014f308345b942463a98268e633d
Paul Blouët <paul@paulblouet.fr>
2020-05-06 16:05:23 +0200

    Release version 2.6.0.

--- src/edf/mod.rs
+++ src/edf/mod.rs
@@ -8,8 +8,8 @@ use serenity::client::Context;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::CommandResult;
 use serenity::model::channel::Message;
-use typemap::Key;
 use serenity::model::id::UserId;
+use typemap::Key;
 
 #[group]
 #[prefix = "edf"]
@@ -36,7 +36,10 @@ fn sing(ctx: &mut Context, msg: &Message) -> CommandResult {
         let followup = {
             let data = ctx.data.read();
             data.get::<SongFollowUpKey>()
-                .filter(|sf| msg.author.id != sf.last_singer && Utc::now() - sf.last_time < Duration::seconds(FOLLOWUP_INTERVAL))
+                .filter(|sf| {
+                    msg.author.id != sf.last_singer
+                        && Utc::now() - sf.last_time < Duration::seconds(FOLLOWUP_INTERVAL)
+                })
                 .map(|sf| sf.song_id)
         };
         let next = followup.map(|id| (id + 1) % SONGS.len()).unwrap_or(0);
