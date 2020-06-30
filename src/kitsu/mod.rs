be18b7037e6cc2b707011e951c05ced702b070c1
Paul Blouët <paul.blouet@outlook.com>
2020-06-30 21:05:35 +0200

    implement greet

--- src/kitsu/mod.rs
+++ src/kitsu/mod.rs
@@ -7,7 +7,13 @@ use serenity::{
 
 mod greet;
 
+const GUILD_ID: GuildId = GuildId(587883211225563160);
+
 #[throws]
-pub fn kitsu_add_member(_ctx: &Context, _guild_member: &(GuildId, Member)) {
-    
+pub fn kitsu_add_member(ctx: &Context, guild_member: &(GuildId, Member)) {
+    let (guild, member) = guild_member;
+    if guild != &GUILD_ID {
+        return;
+    }
+    greet::send_embed(ctx, member)?;
 }
